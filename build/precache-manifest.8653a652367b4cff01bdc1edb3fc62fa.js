0aa775b9b06ca9394521b98c99d9c9fac13ed70b
vijayprabakar32 <111479159+vijayprabakar32@users.noreply.github.com>
2022-09-30 19:55:44 +0530

    Add files via upload

---
+++ build/precache-manifest.8653a652367b4cff01bdc1edb3fc62fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd5fa1a905ead6d72410",
+    "url": "/static/css/main.98c3a0a6.chunk.css"
+  },
+  {
+    "revision": "cd5fa1a905ead6d72410",
+    "url": "/static/js/main.cd5fa1a9.chunk.js"
+  },
+  {
+    "revision": "25acff4685b2caff3160",
+    "url": "/static/css/1.19c69d4d.chunk.css"
+  },
+  {
+    "revision": "25acff4685b2caff3160",
+    "url": "/static/js/1.25acff46.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80145a8212cf23357dbdb6f4c7ba1963",
+    "url": "/index.html"
+  }
+];
